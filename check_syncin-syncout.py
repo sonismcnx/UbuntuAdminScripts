e6b92ca64ec688eeee01995fde941d2fe2a88bfe
sonismcnx <sonismcnx@users.noreply.github.com>
2017-04-07 22:39:19 +0700

    Still not ready yet!

--- check_syncin-syncout.py
+++ check_syncin-syncout.py
@@ -1,17 +1,22 @@
 import os, time
 
-syncinFile = '/tmp/test.txt'
-syncoutFile = '/tmp/test.txt'
-now = int(time.time())
-strNow = time.strftime('%Y-%m-%d %H:%M.%S', time.localtime(now))
-syncinFileEpoch = int(os.path.getmtime(syncinFile))
-syncoutFileEpoch = int(os.path.getmtime(syncoutFile))
-syncinAge = (now - syncinFileEpoch) / 60
-syncoutAge = (now -syncoutFileEpoch) / 60
+def checkSyncinSyncout():
+	try:
+		syncinFile = '/tmp/test.txt'
+		syncoutFile = '/tmp/test.txt'
+		now = int(time.time())
+		strNow = time.strftime('%Y-%m-%d %H:%M.%S', time.localtime(now))
+		syncinFileEpoch = int(os.path.getmtime(syncinFile))
+		syncoutFileEpoch = int(os.path.getmtime(syncoutFile))
+		syncinAge = (now - syncinFileEpoch) / 60
+		syncoutAge = (now -syncoutFileEpoch) / 60
 
-print 'Current Time: ' + strNow
-print "syncin last modified: " + str(time.ctime(os.path.getmtime(syncinFile))) + ", Age: " + str(syncinAge) + " minutes." 
-print "syncout last modified: " + str(time.ctime(os.path.getmtime(syncoutFile))) + ", Age: " + str(syncoutAge) + " minutes."
+		print 'Current Time: ' + strNow
+		print "syncin last modified: " + str(time.ctime(os.path.getmtime(syncinFile))) + ", Age: " + str(syncinAge) + " minutes." 
+		print "syncout last modified: " + str(time.ctime(os.path.getmtime(syncoutFile))) + ", Age: " + str(syncoutAge) + " minutes."
+		return 0
+	except OSError:
+		print "Cannot find the specified file(s)"
+		return 1
 
-
-
+checkSyncinSyncout()
