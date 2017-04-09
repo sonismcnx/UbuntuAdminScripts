a67d83033a2d38f2b252491cd2a66725b6f90f9a
sonismcnx <sonismcnx@users.noreply.github.com>
2017-04-09 19:20:12 +0700

    fix duplicating log lines bug

--- checkLog/CheckSync.py
+++ checkLog/CheckSync.py
@@ -1,55 +1,47 @@
-#!/usr/bin/python
-
+""" Check file age in minutes """
 import os
 import time
 import logging
 
-class CheckSync(object):
+def do_file_check(file_to_check, log_filename):
+    """ the real mccoy """
+    # Get current time
+    now = int(time.time())
+    logger = Logger().get_logger(log_filename)
 
-    """ docstring """
+    try:
+		# Get target files last modification time
+        file_epoch = int(os.path.getmtime(file_to_check))
+    except OSError:
+        logger.critical("Cannot find the specified file")
+        logger.handlers.pop()
+		# Return failed execution
+        return 1
 
-    def __init__(self, file_to_check, log_filename, ):
-        self.file_to_check = file_to_check
-        self.log_filename = log_filename
+	# Compare current with modification time
+    file_age = (now - file_epoch) / 60
 
-    def do_file_check(self):
-        """ the real mccoy """
-		# Get current time
-        now = int(time.time())
+	# Update the log file
+    logger.info(file_to_check + " last modified: " \
+	            + str(time.ctime(os.path.getmtime(file_to_check))) \
+				+ ", Age: " + str(file_age) + " minutes.")
+    logger.handlers.pop()
+	# Return successful execution
+    return 0
 
-		# Initialize logging utility
-        logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
-        logging_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-		# Set up the logger
-        logging_file_handler = logging.FileHandler(self.log_filename)
-        logging_file_handler.setFormatter(logging_formatter)
-        logger.addHandler(logging_file_handler)
+class Logger(object):
+    """ set up the logging utility """
+    logger = None
+    def get_logger(self, log_filename):
+        """ get the logger """
+        if self.logger is None:
+            # Initialize logging utility
+            self.logger = logging.getLogger(log_filename)
+            self.logger.setLevel(logging.INFO)
+            logging_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    	    # Set up the logger
+            logging_file_handler = logging.FileHandler(log_filename)
+            logging_file_handler.setFormatter(logging_formatter)
+            self.logger.addHandler(logging_file_handler)
 
-        try:
-			# Get target files last modification time
-            file_epoch = int(os.path.getmtime(self.file_to_check))
-            # Compare current with modification time
-            file_age = (now - file_epoch) / 60
-
-			# Update the log file
-            logger.info(self.file_to_check + " last modified: " \
-			            + str(time.ctime(os.path.getmtime(self.file_to_check))) \
-						+ ", Age: " + str(file_age) + " minutes.")
-			# Return successful execution
-            return 0
-
-        except OSError:
-            logger.critical("Cannot find the specified file(s)")
-
-			# Return failed execution
-            return 1
-
-    def get_filename(self):
-        """ get the file which you want to check """
-        return self.file_to_check
-
-    def get_logname(self):
-        """ get the path to the log file """
-        return self.log_filename
-		
+        return self.logger
